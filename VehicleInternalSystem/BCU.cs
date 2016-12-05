5b112f54ae87fea97a656d6fc2325a5cd4a15d30
Pedro Loureiro <pedro.loureiro.93@gmail.com>
2016-12-05 15:07:28 +0000

    Basic Solution Implemented
    using RSA Assymetric keys

--- VehicleInternalSystem/BCU.cs
+++ VehicleInternalSystem/BCU.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,6 +19,10 @@ namespace VehicleInternalSystem
 
         private static BinaryReader reader;
         private static BinaryWriter writer;
+
+        private static RSAParameters privateKey;
+        private static RSAParameters publicKey;
+        private static RSAParameters ecuPubKey;
 
         public BCU() { }
 
@@ -62,21 +67,110 @@ namespace VehicleInternalSystem
                 return false;
             }
 
-            response = reader.ReadString();
+            string ecuKey = reader.ReadString();
+            response = GenerateSendKeys(ecuKey);
 
-            if (response.Equals("OK")) return true;
-            else return false;
+            if (response.Equals("OK?"))
+            {
+                writer.Write(EncryptMessage("OK"));
+                return true;
+            }
+            else
+            {
+                reader.Close();
+                writer.Close();
+                bcuSocket.Close();
+                return false;
+            }
 
         }
 
+        public string GenerateSendKeys(string ecuKey)
+        {
+            var csp = new RSACryptoServiceProvider(2048);
+
+            privateKey = csp.ExportParameters(true);
+            //and the public key ...
+            publicKey = csp.ExportParameters(false);
+            ecuPubKey = ConvertStringToKey(ecuKey);
+
+            writer.Write(ConvertKeyToString(publicKey));
+            string encryptedResponse = reader.ReadString();
+
+            return DecryptMessage(encryptedResponse);         
+        }
+
+        public string EncryptMessage(string message)
+        {
+            RSACryptoServiceProvider csp = new RSACryptoServiceProvider();
+
+            byte[] bytesPlainTextData = null;
+            byte[] bytesCypherText = null;
+            string cypherText = null;
+
+            csp.ImportParameters(ecuPubKey);
+            //for encryption, always handle bytes...
+            bytesPlainTextData = System.Text.Encoding.Unicode.GetBytes(message);
+            //apply pkcs#1.5 padding and encrypt our data 
+            bytesCypherText = csp.Encrypt(bytesPlainTextData, false);
+            //we might want a string representation of our cypher text... base64 will do
+            cypherText = Convert.ToBase64String(bytesCypherText);
+
+            return cypherText;
+        }
+
+        public string DecryptMessage(string cypherText)
+        {
+            RSACryptoServiceProvider csp = new RSACryptoServiceProvider();
+
+            byte[] bytesPlainTextData = null;
+            byte[] bytesCypherText = null;
+            string plainTextData = null;
+
+            bytesCypherText = Convert.FromBase64String(cypherText);
+            //we want to decrypt, therefore we need a csp and load our private key
+            csp.ImportParameters(privateKey);
+            //decrypt and strip pkcs#1.5 padding
+            bytesPlainTextData = csp.Decrypt(bytesCypherText, false);
+            //get our original plainText back...
+            plainTextData = System.Text.Encoding.Unicode.GetString(bytesPlainTextData);
+
+            return plainTextData;
+        }
+
+        public string ConvertKeyToString(RSAParameters key)
+        {
+            //we need some buffer
+            var sw = new System.IO.StringWriter();
+            //we need a serializer
+            var xs = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
+            //serialize the key into the stream
+            xs.Serialize(sw, key);
+            //get the string from the stream
+            return sw.ToString();
+        }
+
+        public RSAParameters ConvertStringToKey(string key)
+        {
+            //get a stream from the string
+            var sr = new System.IO.StringReader(key);
+            //we need a deserializer
+            var xs = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
+            //get the object back from the stream
+            RSAParameters pubKey = (RSAParameters)xs.Deserialize(sr);
+            return pubKey;
+        }
+
+
         public string Listen()
         {
-            return reader.ReadString();
+            string response = reader.ReadString();
+            return DecryptMessage(response);
         }
 
         public void Ack()
         {
-            writer.Write("Car Stopped");
+            writer.Write(EncryptMessage("Car Stopped"));
         }
     }
 }
--- VehicleInternalSystem/ECU.cs
+++ VehicleInternalSystem/ECU.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -20,10 +21,23 @@ namespace VehicleInternalSystem
         private TcpClient bcuSocket;
         private static BinaryReader bcuReader;
         private static BinaryWriter bcuWriter;
+        //private key for encrypting from ecu to bcu
+        private static RSAParameters bcu_ecuPriv;
+        //public key to send to bcu
+        private static RSAParameters bcu_ecuPub;
+        //public key to decrypt messages from bcu
+        private static RSAParameters bcuPub;
+
 
         private TcpClient tcuSocket;
         private static BinaryReader tcuReader;
         private static BinaryWriter tcuWriter;
+        //private key for encrypting from ecu to tcu
+        private static RSAParameters tcu_ecuPriv;
+        //public key to send to tcu
+        private static RSAParameters tcu_ecuPub;
+        //public key to decrypt messages from tcu
+        private static RSAParameters tcuPub;
 
         public ECU() { }
 
@@ -45,37 +59,175 @@ namespace VehicleInternalSystem
 
         public string HandleConnection()
         {
-   
-                TcpClient tempSocket = ecuSocket.AcceptTcpClient();
-                BinaryWriter writer = new BinaryWriter(tempSocket.GetStream());
-                BinaryReader reader = new BinaryReader(tempSocket.GetStream());
 
-                writer.Write("ID? ECU");
-                string response = reader.ReadString();
-                switch (response)
-                {
-                    case "BCU":
+            TcpClient tempSocket = ecuSocket.AcceptTcpClient();
+            BinaryWriter writer = new BinaryWriter(tempSocket.GetStream());
+            BinaryReader reader = new BinaryReader(tempSocket.GetStream());
+
+            writer.Write("ID? ECU");
+            string id = reader.ReadString();
+            switch (id)
+            {
+                case "BCU":
+                    
+                    if(KeyDistribution(reader, writer, id))
+                    {
                         bcuSocket = tempSocket;
                         bcuReader = reader;
                         bcuWriter = writer;
-                        bcuWriter.Write("OK");
                         return "CONNECTED TO BCU\n";
+                    }
+                    else return (id + " tried to connect, but was denied\n");
 
-                    case "TCU":
+                case "TCU":
+                    if (KeyDistribution(reader, writer, id))
+                    {
                         tcuSocket = tempSocket;
                         tcuReader = reader;
                         tcuWriter = writer;
-                        tcuWriter.Write("OK");
-                    return "CONNECTED TO TCU";
+                        return "CONNECTED TO TCU";
+                    }
+                    else return (id + " tried to connect, but was denied\n");
             }
 
-                return (response + " tried to connect, but was denied\n");
+            return (id + " tried to connect, but was denied\n");
+        }
+
+        public bool KeyDistribution(BinaryReader reader, BinaryWriter writer, string type)
+        {
+            var csp = new RSACryptoServiceProvider(2048);
+            string response = null;
+
+            switch (type)
+            {
+                case "BCU":
+                    //how to get the private key
+                    bcu_ecuPriv = csp.ExportParameters(true);
+                    //and the public key ...
+                    bcu_ecuPub = csp.ExportParameters(false);
+                    writer.Write(ConvertKeyToString(bcu_ecuPub));
+
+                    string bcuString = reader.ReadString();
+                    bcuPub = ConvertStringToKey(bcuString);
+
+                    writer.Write(EncryptMessage(type, "OK?"));
+                    bcuString = reader.ReadString();
+                    response = DecryptMessage(type, bcuString);
+                    break;
+
+                case "TCU":
+                    //how to get the private key
+                    tcu_ecuPriv = csp.ExportParameters(true);
+                    //and the public key ...
+                    tcu_ecuPub = csp.ExportParameters(false);
+                    writer.Write(ConvertKeyToString(tcu_ecuPub));
+
+                    string tcuString= reader.ReadString();
+                    tcuPub = ConvertStringToKey(tcuString);
+
+                    writer.Write(EncryptMessage(type, "OK?"));
+                    tcuString = reader.ReadString();
+                    response = DecryptMessage(type, tcuString);
+                    break;
+            }
+
+            return response.Equals("OK");
+        }
+
+        public string EncryptMessage(string type, string message)
+        {
+            RSACryptoServiceProvider csp = new RSACryptoServiceProvider();
+
+            byte[] bytesPlainTextData = null;
+            byte[] bytesCypherText = null;
+            string cypherText = null;
+
+            switch (type)
+            {
+                case "BCU":
+                    csp.ImportParameters(bcuPub);
+                    //for encryption, always handle bytes...
+                    bytesPlainTextData = System.Text.Encoding.Unicode.GetBytes(message);
+                    //apply pkcs#1.5 padding and encrypt our data 
+                    bytesCypherText = csp.Encrypt(bytesPlainTextData, false);
+                    //we might want a string representation of our cypher text... base64 will do
+                    cypherText = Convert.ToBase64String(bytesCypherText);
+                    break;
+                case "TCU":
+                    csp.ImportParameters(tcuPub);
+                    //for encryption, always handle bytes...
+                    bytesPlainTextData = System.Text.Encoding.Unicode.GetBytes(message);
+                    //apply pkcs#1.5 padding and encrypt our data 
+                    bytesCypherText = csp.Encrypt(bytesPlainTextData, false);
+                    //we might want a string representation of our cypher text... base64 will do
+                    cypherText = Convert.ToBase64String(bytesCypherText);
+                    break;
+            }
+
+            return cypherText;
+        }
+
+        public string DecryptMessage(string type, string cypherText)
+        {
+            RSACryptoServiceProvider csp = new RSACryptoServiceProvider();
+
+            byte[] bytesPlainTextData = null;
+            byte[] bytesCypherText = null;
+            string plainTextData = null;
+
+            switch (type)
+            {
+                case "BCU":
+                    bytesCypherText = Convert.FromBase64String(cypherText);
+                    //we want to decrypt, therefore we need a csp and load our private key
+                    csp.ImportParameters(bcu_ecuPriv);
+                    //decrypt and strip pkcs#1.5 padding
+                    bytesPlainTextData = csp.Decrypt(bytesCypherText, false);
+                    //get our original plainText back...
+                    plainTextData = System.Text.Encoding.Unicode.GetString(bytesPlainTextData);
+                    break;
+                case "TCU":
+                    bytesCypherText = Convert.FromBase64String(cypherText);
+                    //we want to decrypt, therefore we need a csp and load our private key
+                    csp.ImportParameters(tcu_ecuPriv);
+                    //decrypt and strip pkcs#1.5 padding
+                    bytesPlainTextData = csp.Decrypt(bytesCypherText, false);
+                    //get our original plainText back...
+                    plainTextData = System.Text.Encoding.Unicode.GetString(bytesPlainTextData);
+                    break;
+            }
+
+            return plainTextData;
+        }
+
+        public string ConvertKeyToString(RSAParameters key)
+        {
+            //we need some buffer
+            var sw = new System.IO.StringWriter();
+            //we need a serializer
+            var xs = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
+            //serialize the key into the stream
+            xs.Serialize(sw, key);
+            //get the string from the stream
+            return sw.ToString();
+        }
+
+        public RSAParameters ConvertStringToKey(string key)
+        {
+            //get a stream from the string
+            var sr = new System.IO.StringReader(key);
+            //we need a deserializer
+            var xs = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
+            //get the object back from the stream
+            RSAParameters pubKey = (RSAParameters)xs.Deserialize(sr);
+            return pubKey;
         }
 
         public string ListenTCU()
         {
             string response = tcuReader.ReadString();
-            return response;
+
+            return DecryptMessage("TCU", response);
         }
 
         public string BrakeCmd()
@@ -83,9 +235,9 @@ namespace VehicleInternalSystem
             string response;
             try
             {
-                bcuWriter.Write("BRAKE");
+                bcuWriter.Write(EncryptMessage("BCU","BRAKE"));
                 response = bcuReader.ReadString();
-                return response;
+                return DecryptMessage("BCU", response);
             }
             catch (Exception)
             {
